a5e3ae5f3cc45aeec58dd0d13c356cc9833d3832
ephym-lab <ephymucira9@gmail.com>
2025-10-02 22:56:44 +0300

    some changes in settings.py

--- tafakari/tafakari/settings.py
+++ tafakari/tafakari/settings.py
@@ -232,8 +232,8 @@ SIMPLE_JWT = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.getenv("DEFAULT_FROM_EMAIL")
-EMAIL_HOST_PASSWORD = os.getenv("DEFAULT_FROM_EMAIL_PASSWORD")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
