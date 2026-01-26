dfcc5c29f075df18e91deac583b13d8099d7312b
Ephy Mucira <ephymucira9@gmail.com>
2026-01-26 19:28:54 +0300

    Update settings.py

--- tafakari/tafakari/settings.py
+++ tafakari/tafakari/settings.py
@@ -242,7 +242,7 @@ EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
 
-FRONTEND_URL = "http://localhost:3000"
+FRONTEND_URL="https://kazibudy.netlify.app"
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
