f1bfb3cabeaed07092080a5d5ebf702786c21a2a
ephym-lab <ephymucira9@gmail.com>
2026-02-11 09:53:00 +0300

    some change

--- tafakari/tafakari/settings.py
+++ tafakari/tafakari/settings.py
@@ -34,8 +34,8 @@ DEBUG = os.getenv("DEBUG") =="True"
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
 CORS_ALLOW_CREDENTIALS = os.getenv("CORS_ALLOW_CREDENTIALS")=="True"
 CORS_ALLOW_ALL_ORIGINS = os.getenv("CORS_ALLOW_ALL_ORIGINS")=="True"
-CORS_ALLOWED_ORIGINS=os.getenv("CORS_ALLOWED_ORIGINS").split(",")
-CSRF_TRUSTED_ORIGINS=os.getenv("CSRF_TRUSTED_ORIGINS").split(",")
+CORS_ALLOWED_ORIGINS = [origin.strip() for origin in os.getenv("CORS_ALLOWED_ORIGINS", "").split(",") if origin.strip()]
+CSRF_TRUSTED_ORIGINS = [origin.strip() for origin in os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",") if origin.strip()]
 # Application definition
 
 INSTALLED_APPS = [
@@ -73,7 +73,9 @@ INSTALLED_APPS = [
 SITE_ID = int(os.getenv("SITE_ID", 1))
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  # MUST BE FIRST
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -81,8 +83,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 
