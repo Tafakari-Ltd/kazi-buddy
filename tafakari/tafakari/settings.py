2844821f4d8b31b42c4b736da874593e4a346aea
ephym-lab <ephymucira9@gmail.com>
2026-02-11 10:15:29 +0300

    seeddgf

--- tafakari/tafakari/settings.py
+++ tafakari/tafakari/settings.py
@@ -34,9 +34,9 @@ DEBUG = os.getenv("DEBUG") =="True"
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
 CORS_ALLOW_CREDENTIALS = os.getenv("CORS_ALLOW_CREDENTIALS")=="True"
 CORS_ALLOW_ALL_ORIGINS = os.getenv("CORS_ALLOW_ALL_ORIGINS")=="True"
-CORS_ALLOWED_ORIGINS = [origin.strip() for origin in os.getenv("CORS_ALLOWED_ORIGINS", "").split(",") if origin.strip()]
-CSRF_TRUSTED_ORIGINS = [origin.strip() for origin in os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",") if origin.strip()]
-# Application definition
+# CORS_ALLOWED_ORIGINS = [origin.strip() for origin in os.getenv("CORS_ALLOWED_ORIGINS", "").split(",") if origin.strip()]
+# CSRF_TRUSTED_ORIGINS = [origin.strip() for origin in os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",") if origin.strip()]
+# # Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
