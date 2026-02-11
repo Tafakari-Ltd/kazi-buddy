a8a00439965c2ca1ff305e9880fdb464e36d2e50
ephym-lab <ephymucira9@gmail.com>
2026-02-11 09:40:02 +0300

    cors

--- tafakari/tafakari/settings.py
+++ tafakari/tafakari/settings.py
@@ -35,7 +35,7 @@ ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
 CORS_ALLOW_CREDENTIALS = os.getenv("CORS_ALLOW_CREDENTIALS")=="True"
 CORS_ALLOW_ALL_ORIGINS = os.getenv("CORS_ALLOW_ALL_ORIGINS")=="True"
 CORS_ALLOWED_ORIGINS=os.getenv("CORS_ALLOWED_ORIGINS").split(",")
-
+CSRF_TRUSTED_ORIGINS=os.getenv("CSRF_TRUSTED_ORIGINS").split(",")
 # Application definition
 
 INSTALLED_APPS = [
