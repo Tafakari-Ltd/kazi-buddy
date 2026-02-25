b9db970a1a6a044e5a6440489310a766b71659a3
ephym-lab <ephymucira9@gmail.com>
2026-02-26 02:29:20 +0300

    well defined entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,12 @@
 
 set -e
 
+# Navigate to the Django project directory
+cd /app/tafakari
+
+# Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --noinput --clear
+
 echo "Starting application..."
 exec "$@"
